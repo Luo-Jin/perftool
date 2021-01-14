75ed67c219557dbbe2f66f18db8d2f798914b6b1
Jin Luo <scnjil@eeck8ssmp06.unx.sas.com>
2021-01-14 10:46:07 -0500

    edit

--- srv.sh
+++ srv.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-sudo docker pull scnjil/perftool:v2
-sudo docker run -itd -v `pwd`:/tmp1 -w /tmp1 --rm --name=datacollect scnjil/perftool:v2 bash
+n=`sudo docker image ls | grep scnjil/perftool | wc -l`
+
+if [ $n -lt 1 ]; then
+   sudo docker pull scnjil/perftool:v2
+fi
+
+if [ 'start' = $1 ]; then
+   sudo docker run -itd -v `pwd`:/tmp1 -w /tmp1 --rm --name=datacollect scnjil/perftool:v2 bash
+fi
+
+if [ 'stop' = $1 ]; then
+   sudo docker container stop datacollect
+fi
+
+if [ 'status' = $1 ]; then
+   sudo docker ps | grep datacollect 
+fi
